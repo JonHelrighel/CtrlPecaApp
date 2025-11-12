7e2133b61f2c40987ef9737ba8f252a15aa2dbd2
JonHelrighel <emilianohelrighelfreitas@gmail.com>
2025-11-12 17:19:02 -0300

    V3.1

--- app/src/main/java/br/edu/ifsc/aluno/emilianohelrighel/mobile/ctrlcomputador/model/Peca.java
+++ app/src/main/java/br/edu/ifsc/aluno/emilianohelrighel/mobile/ctrlcomputador/model/Peca.java
@@ -1,7 +1,11 @@
 package br.edu.ifsc.aluno.emilianohelrighel.mobile.ctrlcomputador.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Peca {
     //atributos
+    private int idPeca;
     private String nmPeca;
     private String deMarca;
     private String vlPotencia;
@@ -10,21 +14,69 @@ public class Peca {
     private String vlPolegadas;
     private String vlDpi;
     private int idTipoPeca;
+    //Construtores
 
+    public Peca(){
+        this.nmPeca = "";
+        this.deMarca = "";
+        this.vlPotencia = "";
+        this.deModelo = "";
+        this.vlCapacidade = "";
+        this.vlPolegadas = "";
+        this.vlDpi = "";
+        this.idTipoPeca = 0;
+        this.idPeca = 0;
+    }
 
-    //metodos
+    public Peca(JSONObject jp) throws JSONException {
+        this.setIdTipoPeca(jp.getInt("idTipoPeca"));
+        this.setNmPeca(jp.getString("nmPeca"));
+        this.setDeMarca(jp.getString("deMarca"));
+        this.setVlPotencia(jp.getString("vlPotencia"));
+        this.setDeModelo(jp.getString("deModelo"));
+        this.setVlCapacidade(jp.getString("vlCapacidade"));
+        this.setVlPolegadas(jp.getString("vlPolegadas"));
+        this.setVlDpi(jp.getString("vlDpi"));
+        this.setIdPeca(jp.getInt("idPeca"));
+    }
+    // Metodo retorna o objeto com dados no formato JSON
+
+    public JSONObject toJsonObject() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("idPeca", this.idPeca);
+            json.put("nmPeca", this.nmPeca);
+            json.put("deMarca", this.deMarca);
+            json.put("vlPotencia", this.vlPotencia);
+            json.put("deModelo", this.deModelo);
+            json.put("vlCapacidade", this.vlCapacidade);
+            json.put("vlPolegadas", this.vlPolegadas);
+            json.put("vlDpi", this.vlDpi);
+            json.put("idTipoPeca", this.idTipoPeca);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    public int getIdPeca() {
+        return idPeca;
+    }
+
+    public void setIdPeca(int idPeca) {
+        this.idPeca = idPeca;
+    }
 
     public String getNmPeca() {
         return nmPeca;
     }
 
-    public void setNmPeca(String nmPeca) throws Exception {
+    public void setNmPeca(String nmPeca) {
         if (nmPeca.length() > 2) {
             this.nmPeca = nmPeca;
         } else {
-            throw new Exception("Nome muito curto");
+            throw new IllegalArgumentException("Nome muito curto");
         }
-
     }
 
     public String getDeMarca() {
--- app/src/main/java/br/edu/ifsc/aluno/emilianohelrighel/mobile/ctrlcomputador/ui/tipopeca/CadTipopecaFragment.java
+++ app/src/main/java/br/edu/ifsc/aluno/emilianohelrighel/mobile/ctrlcomputador/ui/tipopeca/CadTipopecaFragment.java
@@ -1,66 +1,159 @@
 package br.edu.ifsc.aluno.emilianohelrighel.mobile.ctrlcomputador.ui.tipopeca;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import br.edu.ifsc.aluno.emilianohelrighel.mobile.ctrlcomputador.R;
+import br.edu.ifsc.aluno.emilianohelrighel.mobile.ctrlcomputador.model.Peca;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CadTipopecaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CadTipopecaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "CadTipopecaFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Declaração das views do XML
+    private EditText etNmPeca;
+    private EditText etDeMarca;
+    private EditText etVlPotencia;
+    private EditText etDeModelo;
+    private EditText etVlCapacidade;
+    private EditText etVlPolegadas;
+    private EditText etVlDpi;
+    private Spinner spIdTipoPeca;
+    private Button btnSalvar;
 
     public CadTipopecaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CadTipopecaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CadTipopecaFragment newInstance(String param1, String param2) {
-        CadTipopecaFragment fragment = new CadTipopecaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        Log.d(TAG, "onCreate chamado");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView chamado");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cad_tipopeca, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated chamado");
+
+        // Associando as views do XML aos objetos Java
+        etNmPeca = view.findViewById(R.id.etNmPeca);
+        etDeMarca = view.findViewById(R.id.etDeMarca);
+        etVlPotencia = view.findViewById(R.id.etVlPotencia);
+        etDeModelo = view.findViewById(R.id.etDeModelo);
+        etVlCapacidade = view.findViewById(R.id.etVlCapacidade);
+        etVlPolegadas = view.findViewById(R.id.etVlPolegadas);
+        etVlDpi = view.findViewById(R.id.etVlDpi);
+        spIdTipoPeca = view.findViewById(R.id.spIdTipoPeca);
+        btnSalvar = view.findViewById(R.id.btnSalvar);
+
+        // Configurando o listener para o botão Salvar
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salvarPeca();
+            }
+        });
+    }
+
+    private void salvarPeca() {
+        try {
+            // Criando um novo objeto Peca
+            Peca peca = new Peca();
+
+            // Definindo os valores dos EditTexts e Spinner
+            peca.setNmPeca(etNmPeca.getText().toString().trim());
+            peca.setDeMarca(etDeMarca.getText().toString().trim());
+            peca.setVlPotencia(etVlPotencia.getText().toString().trim());
+            peca.setDeModelo(etDeModelo.getText().toString().trim());
+            peca.setVlCapacidade(etVlCapacidade.getText().toString().trim());
+            peca.setVlPolegadas(etVlPolegadas.getText().toString().trim());
+            peca.setVlDpi(etVlDpi.getText().toString().trim());
+
+            // Para idTipoPeca, assumindo que o Spinner usa posições como IDs (ex.: posição 0 = ID 1, ajuste se necessário)
+            int idTipoPeca = spIdTipoPeca.getSelectedItemPosition() + 1; // +1 se IDs começam em 1
+            peca.setIdTipoPeca(idTipoPeca);
+
+            // Aqui você pode salvar no banco, enviar para API, etc. Por enquanto, só logamos o JSON
+            Log.d(TAG, "Peca salva: " + peca.toJsonObject().toString());
+
+            // Mostrando uma mensagem de sucesso
+            Toast.makeText(getContext(), "Peça salva com sucesso!", Toast.LENGTH_SHORT).show();
+
+            // Limpar os campos após salvar (opcional)
+            limparCampos();
+
+        } catch (IllegalArgumentException e) {
+            // Tratando erros de validação (ex.: nome curto)
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            // Outros erros
+            Log.e(TAG, "Erro ao salvar peça", e);
+            Toast.makeText(getContext(), "Erro ao salvar peça", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void limparCampos() {
+        etNmPeca.setText("");
+        etDeMarca.setText("");
+        etVlPotencia.setText("");
+        etDeModelo.setText("");
+        etVlCapacidade.setText("");
+        etVlPolegadas.setText("");
+        etVlDpi.setText("");
+        spIdTipoPeca.setSelection(0);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart chamado");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume chamado");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause chamado");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop chamado");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView chamado");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy chamado");
     }
 }
